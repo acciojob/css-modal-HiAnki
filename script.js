d6fcb8ea1f0af0bde1aa3e4f292d8e6a0f4759cb
Ankita Pathak <101946629+HiAnki@users.noreply.github.com>
2024-02-06 12:17:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+    let popup = document.getElementById("modal");
+    
+    function openPopup() {
+        popup.classList.add("open-popup");
+        document.addEventListener('click', clickOutsidePopup);
+    }
+    function closePopup() {
+        popup.classList.remove("open-popup");
+        document.removeEventListener('click', clickOutsidePopup);
+    }
+    function clickOutsidePopup(event) {
+        if (popup.contains(event.target)) {
+            closePopup();
+        }
+    }
